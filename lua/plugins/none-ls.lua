b8d2f6616d501ce1286fa40f1783d8e7fc6bc1a5
NicholasTanYY <nicholastanyunyu@gmail.com>
2024-07-22 13:09:29 +0800

    Modified certain configuration to remove errors

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,18 +1,25 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.diagnostics.erb_lint,
-				null_ls.builtins.diagnostics.eslint_d,
-				null_ls.builtins.diagnostics.rubocop,
-				null_ls.builtins.formatting.rubocop,
-			},
-		})
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        --require("none-ls.diagnostics.eslint"),
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettierd,
+        null_ls.builtins.diagnostics.djlint,
+        null_ls.builtins.diagnostics.stylelint,
+        -- null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.diagnostics.dotenv_linter,
+        null_ls.builtins.diagnostics.markdownlint,
+        null_ls.builtins.diagnostics.textlint,
+        null_ls.builtins.diagnostics.cppcheck,
+      },
+    })
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
 }
