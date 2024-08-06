d8b67b94b505f95a040043d6535fc5fae9e44cae
NicholasTanYY <nicholastanyunyu@gmail.com>
2024-08-06 22:36:40 +0800

    Add python formatter and linter

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -49,6 +49,10 @@ return {
                     },
                 }
             })
+            lspconfig.pyright.setup({
+                capabilities = capabilities
+            })
+
 
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,24 +1,26 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
-  },
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        --require("none-ls.diagnostics.eslint"),
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettierd,
-        null_ls.builtins.diagnostics.djlint,
-        null_ls.builtins.diagnostics.stylelint,
-        -- null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.diagnostics.dotenv_linter,
-        null_ls.builtins.diagnostics.markdownlint,
-        null_ls.builtins.diagnostics.cppcheck,
-      },
-    })
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				--require("none-ls.diagnostics.eslint"),
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettierd,
+				null_ls.builtins.diagnostics.djlint,
+				null_ls.builtins.diagnostics.stylelint,
+				-- null_ls.builtins.diagnostics.eslint,
+				null_ls.builtins.diagnostics.dotenv_linter,
+				null_ls.builtins.diagnostics.markdownlint,
+				null_ls.builtins.diagnostics.cppcheck,
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.isort,
+			},
+		})
 
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
 }
